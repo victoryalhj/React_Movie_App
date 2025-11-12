7d2e202859e1d24a387d5a3f435cab15e62ccfce
victoryalhj <victoryalhj@gmail.com>
2025-11-12 21:11:39 +0000

    fix: fix error of genre

--- src/common/MovieCard/MovieCard.jsx
+++ src/common/MovieCard/MovieCard.jsx
@@ -4,14 +4,18 @@ import "./MovieCard.style.css";
 import { useMovieGenreQuery } from "../../hooks/useMovieGenre";
 
 const MovieCard = ({ movie }) => {
-  const { data: genreData } = useMovieGenreQuery();
-  if(!movie||!genreData) return null;
+  const { data: genreData} = useMovieGenreQuery();
+
+  console.log("movie title", movie?.title)
+  console.log("genre ids",movie?.genre_ids)
+
+  if(!movie ||!genreData) return null;
 
   const showGenre = (genreIdList) => {
-    if (!genreData.genres) return [];
+    if (!Array.isArray(genreData) || genreData.length === 0) return [];
 
     const genreNameList = genreIdList.map((id) => {
-      const genreObj = genreData.genres.find((genre) => genre.id === id);
+      const genreObj = genreData.find((genre) => genre.id === id);
       return genreObj? genreObj.name: "Unknown";
     });
     return genreNameList;
